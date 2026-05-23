661d7f1f384df9bbb5fe9593298381884786b19a
haribansh2121 <haribansh.singh2113@gmail.com>
2026-05-23 23:02:29 +0400

    installed mysql1

--- backend/src/main/java/com/salonflow/backend/repository/TenantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salonflow.backend.repository;
-
-import com.salonflow.backend.entity.Tenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TenantRepository extends JpaRepository<Tenant, String> {
-}
